d61b03dc4ad4fcf2e2045f09a64ec293ecce1de8
João Pedro Silva <joaopedrogmsilva@icloud.com>
2025-11-11 11:05:42 -0300

    Adicionado: colisões nas janelas laterais a fim de criar uma função para olhar pela janela

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -19,8 +19,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 # Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Não acho que será necessário pulo na gameplay
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
 
 # Get the input direction and handle the movement/deceleration.
 # As good practice, you should replace UI actions with custom gameplay actions.
